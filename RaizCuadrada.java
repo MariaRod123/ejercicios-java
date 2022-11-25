0c42e6b3ffe621aa04aeb635c2d012f1e2dedca6
María <72228855+MariaRod123@users.noreply.github.com>
2022-11-25 15:13:31 -0300

    Create RaizCuadrada.java

---
+++ RaizCuadrada.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class RaizCuadrada {
+
+    public static void main(String[] args) {
+
+        /* Programa que calcula la raiz cuadrada de un número */
+
+        //variables
+
+        float radicando;
+        float raiz;
+        int indice=2;
+
+        String error="No existe la raíz cuadrada de número negativo en el campo de los números reales";
+
+        System.out.println("Ingrese un valor");
+        Scanner input= new Scanner(System.in);
+        radicando=input.nextInt();
+
+
+        if(radicando<0 && (indice%2==0)) {
+            System.out.println(error);
+        }
+        else if(radicando>=0 ){
+            raiz= (float) Math.sqrt(radicando);
+            System.out.println("El resultado es: " + raiz);
+
+        }
+
+
+        }
+    }
+
+
+
+
+
