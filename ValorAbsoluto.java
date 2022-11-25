169b4a037ab27147c7b1eb83e64c74a120178988
María <72228855+MariaRod123@users.noreply.github.com>
2022-11-25 15:35:41 -0300

    Create ValorAbsoluto.java

---
+++ ValorAbsoluto.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ValorAbsoluto {
+    public static void main(String[] args) {
+
+        /* programa que calcula el valor absoluto de un número
+         
+        Definición :  |x|= x si x es un número real distinto de 0 ; si x=0 --> |x|=0
+
+        */
+
+        int valor;
+        int valor_absoluto;
+
+        System.out.println("Ingrese un número");
+        Scanner input= new Scanner(System.in);
+        valor=input.nextInt();
+
+        if (valor!=0){
+            valor_absoluto=Math.abs(valor);
+            System.out.println("|" + valor + "| = " + valor_absoluto );
+        }
+        else{
+            valor_absoluto=0;
+            System.out.println("|" + valor + "| = " + valor_absoluto );
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
